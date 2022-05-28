ebeae3d82d7b5851446668642b551c17c90c7541
Ivan Cordova <talivan1602@gmail.com>
2022-05-28 00:21:36 -0500

    Consulta alumno materias tomadas terminada

--- Controllers/materiaController.js
+++ Controllers/materiaController.js
@@ -1,19 +1,35 @@
 const { response, request } = require("express")
-const {MateriaModel, TareaModel, AlumnoModel} = require("../Models/indexModel")
+const { MateriaModel, TareaModel, AlumnoModel, Alumno_materiaModel } = require("../Models/indexModel")
+const { Op, where } = require("sequelize");
+const {db} = require("../Database/connect")
 
 const getAllMaterias = async (req = request, res = response) => {
     try {
         const materias = await MateriaModel.findAll({
             include: [{
                 model: TareaModel
-            },{
+            }, {
                 model: AlumnoModel
             }]
         })
         res.status(200).json(materias)
     } catch (error) {
-        res.status(500).json({message: error.message})
-        
+        res.status(500).json({ message: error.message })
+
+    }
+}
+
+
+// Materias que no toma el alumno
+const getAllMaterias_alumno = async (req = request, res = response) => {
+    try {
+        const id = req.params.id
+        const materias = db.query(`SELECT * from materia WHERE id NOT IN (SELECT id_materia from alumno_materia WHERE id_alumno = ${id})`).then(results => {
+            res.status(200).json(results[0])
+        });
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+
     }
 }
 
@@ -21,18 +37,18 @@ const getMateria = async (req = request, res = response) => {
     try {
         const id = req.params.id
         const materia = await MateriaModel.findAll({
-            where:{id}
+            where: { id }
         })
-        if(materia == 0){
+        if (materia == 0) {
             return res.status(400).json({
-                msg :'No se encontro la materia'
+                msg: 'No se encontro la materia'
             });
-        }else{
+        } else {
 
             res.status(200).json(materia)
         }
     } catch (error) {
-        res.status(500).json({message: error.message})
+        res.status(500).json({ message: error.message })
     }
 }
 
@@ -44,33 +60,33 @@ const createMateria = async (req = request, res = response) => {
             "message": "¡Registro creado correctamente!"
         })
     } catch (error) {
-        res.status(500).json({message: error.message})
+        res.status(500).json({ message: error.message })
     }
 }
 
 const updateMateria = async (req = request, res = response) => {
     try {
-        await MateriaModel.update(req.body,{
-            where:{id: req.params.id}
+        await MateriaModel.update(req.body, {
+            where: { id: req.params.id }
         })
         res.status(200).json({
             "message": "¡Registro actualizado correctamente!"
         })
     } catch (error) {
-        res.status(500).json({message: error.message})
+        res.status(500).json({ message: error.message })
     }
 }
 
 const deleteMateria = async (req = request, res = response) => {
     try {
         await MateriaModel.destroy({
-            where: {id: req.params.id}
+            where: { id: req.params.id }
         })
         res.status(200).json({
             "message": "¡Registro eliminado correctamente!"
         })
     } catch (error) {
-        res.status(500).json({message: error.message})
+        res.status(500).json({ message: error.message })
     }
 }
 
@@ -79,5 +95,6 @@ module.exports = {
     getMateria,
     createMateria,
     updateMateria,
-    deleteMateria
+    deleteMateria,
+    getAllMaterias_alumno
 }
--- Routes/materiaRoute.js
+++ Routes/materiaRoute.js
@@ -1,10 +1,11 @@
 const { Router } = require("express")
-const { getAllMaterias, getMateria, createMateria, updateMateria, deleteMateria } = require("../Controllers/materiaController")
+const { getAllMaterias, getMateria, createMateria, updateMateria, deleteMateria, getAllMaterias_alumno } = require("../Controllers/materiaController")
 
 const router = Router()
 
 router.get('/', getAllMaterias)
 router.get('/:id', getMateria)
+router.get('/alumno/:id', getAllMaterias_alumno)
 router.post("/",createMateria),
 router.put('/:id', updateMateria)
 router.delete("/:id", deleteMateria)
