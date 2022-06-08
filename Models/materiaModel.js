797ed1e38144d79b3102abe98286c04d22725e67
Ivan Cordova <talivan1602@gmail.com>
2022-06-08 15:03:32 -0500

    Cambios 8/6/22

--- Models/materiaModel.js
+++ Models/materiaModel.js
@@ -4,7 +4,8 @@ const { db } = require("../Database/connect")
 const MateriaModel = db.define("materia",{
     id: {type: DataTypes.INTEGER, autoIncrement: true,primaryKey: true},
     nombre: {type: DataTypes.STRING, allowNull: false},
-    descripcion: {type: DataTypes.INTEGER, allowNull: false}
+    descripcion: {type: DataTypes.INTEGER, allowNull: false},
+    imagen: {type: DataTypes.STRING, allowNull: false}
 },{
     tableName: "materia"
 })
